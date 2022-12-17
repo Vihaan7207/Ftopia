89e8d73dbdb42ef5f2ef0e2592d68bf19ed95ae4
Vihaan Mikkilineni <vmikki7207@gmail.com>
2022-12-17 09:26:44 -0500

    commiting autonomously

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from helpers.colors import fg, util
 from helpers.snippets import prompts
 from helpers.functions import typePrint
+print(util.clear)
 print(f'''{fg.blue}{util.bold}
 ███████╗████████╗░█████╗░██████╗░██╗░█████╗░
 ██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██║██╔══██╗
@@ -12,4 +13,6 @@ print(f'''{fg.blue}{util.bold}
 input(prompts.enter)
 typePrint(f'{fg.green}{util.bold}Welcome to Ftopia, a magical land full of mystical wonders\n{util.reset}')
 input(prompts.enter)
-typePrint(f'{fg.green}{util.bold}You have just arrived in this world, and you will embark on an amazing journey in the days to come{util.reset}')
+typePrint(f'{fg.green}{util.bold}You have just arrived in this world, and you will embark on an amazing journey in the days to come\n{util.reset}')
+input(prompts.enter)
+print(util.clear)
