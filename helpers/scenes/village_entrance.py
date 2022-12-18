d56e654fa11aca99083012f5e35c56d828549376
Vihaan Mikkilineni <vmikki7207@gmail.com>
2022-12-18 09:37:24 -0500

    commiting autonomously

--- helpers/scenes/village_entrance.py
+++ helpers/scenes/village_entrance.py
@@ -1,5 +1,9 @@
 from helpers.functions import printChoices
 from helpers.colors import fg
+import helpers.scenes.start
 
 def main():
-    printChoices('You walk to the entrance to the village. There is a gate with two guards')
+    printChoices('You walk to the entrance to the village. There is a gate with two guards.\nYou can:', ['Ask the guards to enter', 'Look for a different entrance', 'Go back'])
+    choice = input('> ')
+    if choice == '3':
+        helpers.scenes.start.main()
